c55b0394a95b57a520fbe37efa4e80842c89001f
wonwoo <aoruqjfu@naver.com>
2015-12-14 21:40:28 +0900

    search api 추가

--- src/main/java/org/springframework/social/daum/api/SearchOperations.java
+++ src/main/java/org/springframework/social/daum/api/SearchOperations.java
@@ -3,4 +3,13 @@ package org.springframework.social.daum.api;
 public interface SearchOperations {
 
 	DaumSearch<DaumBlogSearch> board(String q, Integer result, String advance, Integer pageno, String sort);
+
+	DaumSearch<DaumBlogSearch> board(String q, Integer result, String advance, Integer pageno);
+
+	DaumSearch<DaumBlogSearch> board(String q, Integer result, String advance);
+
+	DaumSearch<DaumBlogSearch> board(String q, Integer result);
+
+	DaumSearch<DaumBlogSearch> board(String q);
+
 }
